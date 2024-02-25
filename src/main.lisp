7d92874917982a17f2e1eb603a4aa248f6620865
Max Mäteling <max@gaw2007.com>
2024-02-25 21:22:16 +0100

    WIP

--- src/main.lisp
+++ src/main.lisp
@@ -1,13 +1,22 @@
 (defpackage cl-training
-  (:use :cl))
+  (:use :cl :cl-training.parsers :maxpc))
 (in-package :cl-training)
 
 (defparameter *log-path* #p"/home/max/projects/lisp/training/data/training.log")
 (defparameter *program-path* #p"/home/max/projects/lisp/training/data/program.log")
 
+(defun load-parse-training (&optional (path *log-path*))
+  (with-open-file (s path)
+	(parse s (=trainings))))
+
 (defun interactive-log-training ())
 (defun interactive-log-exercise ())
 
+(defun 1rm (weight reps)
+  "Calculate 1 repetition max based on weights anc reps according to Brzycki formula from https://en.wikipedia.org/wiki/One-repetition_maximum"
+  (/ weight (- (/ 37 36) (/ reps 36))))
 
-(defun tonnage (exercise))
+(defun tonnage (sets reps weight)
+  (* sets reps weight))
+
 (defun max-1rm (exercise))
