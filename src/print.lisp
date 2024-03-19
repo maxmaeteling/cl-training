57b32de80db084917871f030942a6d369cceb708
Max Mäteling <max@gaw2007.com>
2024-03-19 12:02:08 +0100

    Minor refactoring

--- src/print.lisp
+++ src/print.lisp
@@ -6,28 +6,26 @@
 (defun output-date (stream date-list)
   (format stream "~{~2,'0d~^/~}~%" date-list))
 
-(defmethod output-set-readable (stream set)
-  (break)
-  (format stream "skipped"))
-
 (defmethod output-set-readable (stream (set exercise-set))
   (format stream "~{~d~^,~}" (set-reps set)))
 
 (defmethod output-set-readable (stream (set set-weight))
-  (format stream "~dx~d" (set-reps set) (set-weight set)))
+  (format stream "~d*~a" (set-reps set) (set-weight set)))
 
 (defmethod output-set-readable (stream (set multi-set-weight))
-  (format stream "~dx~dx~a" (set-number set) (set-reps set) (set-weight set)))
+  (format stream "~d*~d*~a" (set-number set) (set-reps set) (set-weight set)))
 
 (defun output-exercise-readable (stream exercise)
-  (format stream "~a: " (exercise-name exercise))
-  (format stream "~{~a ~}~%" (mapcar #'(lambda (set) (output-set-readable nil set)) 
-								   (exercise-sets exercise))))
+  (format stream
+		  "~a: ~{~a ~}~%"
+		  (exercise-name exercise)
+		  (mapcar #'(lambda (set) (output-set-readable nil set)) 
+				  (exercise-sets exercise))))
 
 (defun output-readable (log &optional (stream nil))
   (loop
 	for training in log
-	do (output-date stream (training-date training))
-	do (mapcar #'(lambda (ex) (output-exercise-readable stream ex))
-			   (training-exercises training))
-	do (format stream "~%")))
+	do (progn (output-date stream (training-date training))
+			  (mapcar #'(lambda (ex) (output-exercise-readable stream ex))
+					  (training-exercises training))
+			  (format stream "~%"))))
